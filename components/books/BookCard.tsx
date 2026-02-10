101af18f05fa88e3f73874128bbdafe45a7bb075
Rodrigo FC <rodrigofagundescoruja@gmail.com>
2026-02-09 23:22:29 -0300

    ajuste efeito hover e line clamp book card

--- components/books/BookCard.tsx
+++ components/books/BookCard.tsx
@@ -7,8 +7,10 @@ interface Props {
 
 export function BookCard({ book }: Props) {
   return (
-    <div className="group cursor-pointer h-96 rounded-lg border-2 border-transparent hover:border-neutral-300 hover:shadow-lg transition-all duration-200
-">
+    <div className="group cursor-pointer h-96 rounded-lg 
+      border-2 border-transparent 
+      hover:border-neutral-300 hover:shadow-lg transition-all duration-200 overflow-hidden
+    ">
       <div className="relative h-64 aspect-3/4 overflow-hidden mb-3 group-hover:rounded-t-lg">
         <Image
           src={book.coverUrl}
@@ -20,15 +22,15 @@ export function BookCard({ book }: Props) {
       </div>
 
       <div className="flex flex-col gap-3 px-2">
-        <h3 className="font-medium text-base leading-tight w-full h-10 text-center line-clamp-2 group-hover:text-lime-900 transition-colors">
+        <h3 className="font-medium tracking-tight text-base leading-tight w-full h-10 text-center line-clamp-2 group-hover:text-lime-900 transition-colors">
           {book.title}
         </h3>
 
         <div className="h-full flex flex-col justify-between">
-          <p className="text-sm w-full text-center line-clamp-2 text-neutral-600 group-hover:text-lime-900 transition-colors">
+          <p className="font-normal tracking-tight text-sm w-full text-center line-clamp-1 text-neutral-600 group-hover:text-lime-900 transition-colors">
             {book.author}
           </p>
-          <p className="text-sm w-full text-center text-neutral-500 group-hover:text-lime-900 transition-colors">
+          <p className="font-normal tracking-tight text-sm w-full text-center text-neutral-500 group-hover:text-lime-900 transition-colors">
             {book.pagesNumber} páginas • R$ {book.pricePerDay}/dia
           </p>
         </div>
